3cbd03a2afd2f84b2017dec9a6f5ce5741c00dcc
JHRunning <jeonghunn1@gmail.com>
2013-12-23 02:26:48 +0900

    Beta r4 : Member_info updated

--- member/member_info.php
+++ member/member_info.php
@@ -4,10 +4,14 @@ $authcode = $_POST['authcode'];
 $user_srl = addslashes($_POST['user_srl']);
 $user_srl_auth = addslashes($_POST['user_srl_auth']);
 $lang = addslashes($_POST['lang']);
+$member_info = addslashes($_POST['member_info']);
 
 define('642979',   TRUE);
 require '../db.php';
 //mysql_select_db('favorite',$db_conn);
+
+//Check Permission
+if($authcode != $auth) exit();
 
 //Auth code to user_srl
 require '../auth.php';
@@ -17,26 +21,32 @@ $user_srl = AuthCheck($user_srl_auth, false);
    $add_info_to_system ="UPDATE `user` SET  `lang` = '$lang' ,`ip_addr` = '$REMOTE_ADDR' WHERE `user_srl` = $user_srl";
             $system_result = mysql_query($add_info_to_system);
 
-// Set UTF-8
 
-// mysql_query("set session character_set_connection=utf8;");
-// mysql_query("set session character_set_results=utf8;");
-// mysql_query("set session character_set_client=utf8;");
-
-
-//Check Permission
-if($authcode == $auth) {
-
+//Cut the value
+            $member_info_array = explode("//",$member_info);
 
 
          //Send Imformation
         $sql ="SELECT * FROM  `user` WHERE  `user_srl` LIKE '$user_srl'";
         $result = mysql_query($sql);
         $row=mysql_fetch_array($result);
-        echo "$row[tarks_account]/LINE/.$row[name_1]/LINE/.$row[name_2]/LINE/.$row[permission]/LINE/.$row[reg_id]/LINE/.$row[key]/LINE/.$row[like_me]/LINE/.$row[favorite]";
-    
-    
+
+// while (count($member_info_array)) {
+
+// }
+
+   for ($i=0 ; $i < count($member_info_array);$i++){
+   	if(count($member_info_array) == $i + 1){
+echo $row[$member_info_array[$i]];
+}else{
+	echo $row[$member_info_array[$i]]."/LINE/.";
+   }
+
+    //    echo "$row[tarks_account]/LINE/.$row[name_1]/LINE/.$row[name_2]/LINE/.$row[permission]/LINE/.$row[reg_id]/LINE/.$row[key]/LINE/.$row[like_me]/LINE/.$row[favorite]";
 }
+    
+    
+
    
       
 ?>
