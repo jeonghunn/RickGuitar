fc7a680002774828d7cf121a99ad87d61184c8e3
JHRunning <jeonghunn1@gmail.com>
2014-01-04 03:27:26 +0900

    Beta r11 : upload.php added

---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+$target_path = "files/profile/";
+
+$tmp_img = explode("." ,$_FILES['uploadedfile']['name']); 
+$img_name = $tmp_img[0]."_".date('Y-m-d_H_i_s').".".$tmp_img[1];
+$target_path = $target_path . basename($img_name);
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+	echo "The file ".$img_name." has been uploaded";
+} else {
+	echo "There was an error uploading the file, please try again!";
+}
+
+?>
