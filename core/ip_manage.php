9134c528b0e516b66185e2b724ee57477d73651d
JHRunning <jeonghunn1@gmail.com>
2014-03-09 01:48:27 +0900

    Beta r28 : ip manage updated

--- core/ip_manage.php
+++ core/ip_manage.php
@@ -12,6 +12,7 @@
 	//Check DDOS
 	if($ip_point > 99) $ip_active = "N";
 	if($ip_manage[last_access] > $date - 2) $ip_point = $ip_point + 1;
+	if($ip_manage[last_access] < $date - 10000 && $ip_point != 0) $ip_point = $ip_point - 1;
 	//Information Update
 	mysql_query("UPDATE `ip_manage` SET  `active` = '$ip_active', `point` = '$ip_point' , `last_access` = '$date' WHERE `ip_addr` = '$REMOTE_ADDR'");
 }
