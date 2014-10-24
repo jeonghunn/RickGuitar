074ba31d1c98cabceac275af07209e88de0eec86
JHRunning <jeonghunn1@gmail.com>
2014-10-25 01:16:32 +0900

    2.9.0.82 : ver name fix

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $REMOTE_ADDR  = $_SERVER["REMOTE_ADDR"];
 $nowurl = $_SERVER["REQUEST_URI"]; 
 $useragent = $_SERVER['HTTP_USER_AGENT'];
 $date = strtotime(date('Y-m-d H:i:s'));
-$server_version = "2.9.3.82";
+$server_version = "2.9.0.82";
 
 //Language
 $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
