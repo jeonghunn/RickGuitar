49f3a041b57ccec7bbc954a4ed9f1a76a84b824c
JHRunning <jeonghunn1@gmail.com>
2014-12-26 02:22:56 +0900

    css updated

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $REMOTE_ADDR  = $_SERVER["REMOTE_ADDR"];
 $nowurl = $_SERVER["REQUEST_URI"]; 
 $useragent = $_SERVER['HTTP_USER_AGENT'];
 $date = strtotime(date('Y-m-d H:i:s'));
-$SERVER_VERSION = "2.15.0.97";
+$SERVER_VERSION = "2.15.1.98";
 
 //Language
 $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
