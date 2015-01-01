fb8d39dfc47064663543c01ab517133b5f64704f
JHRunning <jeonghunn1@gmail.com>
2015-01-01 16:25:46 +0900

    info updated

--- pages/info.php
+++ pages/info.php
@@ -4,10 +4,8 @@
 
   <center>  <h1 style="font-size: 36px; center;color: #fd9800;">FavoriteCore</h1>
 <br><br>
-      <h3 stlye="font-size: 21px;"><? P($CORE_VERSION) ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
+      <h3 stlye="font-size: 21px;">456456456</h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
 
-<!-- <div style="margin-top: 15%; margin-left: 30%; margin-right:30%;">
- <button type="button" class="btn btn-success btn-lg btn-block"><? S('add_api_key')?></button><br>
-  <button type="button" class="btn btn-primary btn-lg btn-block" ><? S('manage_api')?></button> -->
+
   </div>
 </center>
