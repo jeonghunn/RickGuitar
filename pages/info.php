d1f8273486efcaeff860115c663cf46c1f37a38a
JHRunning <jeonghunn1@gmail.com>
2015-01-01 16:33:24 +0900

    info updated

--- pages/info.php
+++ pages/info.php
@@ -4,7 +4,7 @@
 
   <center>  <h1 style="font-size: 36px; center;color: #fd9800;">FavoriteCore</h1>
 <br><br>
-      <h3 stlye="font-size: 21px;"><? P($CORE_VERSION); if($CORE_VERSION == null) echo "dfajkdlsfjaklsjfkl"; ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
+      <h3 stlye="font-size: 21px;"><? P($nowurl) ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
 
 
   </div>
