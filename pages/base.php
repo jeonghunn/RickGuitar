a620b680efc9f0749651bfff49bc03f5696f6cbb
JHRunning <jeonghunn1@gmail.com>
2015-01-04 01:22:29 +0900

    info updated

--- pages/base.php
+++ pages/base.php
@@ -96,7 +96,7 @@ $vars =+ $arrayvars[$i][0]."=".$arrayvars[$i][1];
 
 $ch = curl_init( $url );
 curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $vars);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, "a=CoreVersion");
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt( $ch, CURLOPT_HEADER, 0);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
