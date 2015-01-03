782b308216ad73db0c4aa63f3f86fb835b6f5f55
JHRunning <jeonghunn1@gmail.com>
2015-01-04 01:39:09 +0900

    Info updated

--- pages/info.php
+++ pages/info.php
@@ -4,7 +4,7 @@
 
   <center>  <h1 style="font-size: 36px; center;color: #fd9800;">FavoriteCore</h1>
 <br><br>
-      <h3 stlye="font-size: 21px;" ><? echo PostAct('http://tarks.net/develop/favorite/api.php',  array(array('a', 'CoreVersion'))); ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
+      <h3 stlye="font-size: 21px;" ><? echo PostAct('api.php',  array(array('a', 'CoreVersion'))); ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
 
 
   </div>
