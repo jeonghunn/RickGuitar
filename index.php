7078448e11559c239df48a7646b3061d9103d1ec
JHRunning <jeonghunn1@gmail.com>
2015-01-09 15:23:37 +0900

    index json part updated

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $loaded = false;
 //Session
 session_start();
 $user_auth = $_SESSION['user_auth'];
-echo $user_auth;
 //Lang
 
 require_once "pages/lang/".getLang().".php";
@@ -31,7 +30,7 @@ require_once "pages/lang/".getLang().".php";
 //require_once 'core/api.class.php';
 
 $user_info = json_decode(PostAct(getAPIUrl(),  array(array('a', 'my_page_info'), array('apiv', getAPIVersion()), array('api_key', getAPIKey()), array('user_auth', $user_auth), array('page_info', 'tarks_account//admin//name_1//name_2//gender//birthday//country_code//phone_number//join_day//profile_pic//profile_update//lang//country//like_me//favorite//rel_you_status//rel_me_status'))));
-echo $user_info;
+echo $user_info['tarks_account'];
 
 //$user_srl = $user_info['user_srl'];
 //$user_name = SetUserName($user_info['lang'], $user_info['name_1'], $user_info['name_2']);
--- pages/base.php
+++ pages/base.php
@@ -5,7 +5,7 @@
 //    return "http://tarks.net/develop/favorite/";
 //}
 function getClientVersion(){
-    return "0.1.14.0.47";
+    return "0.1.14.1.48";
 }
 
 function getAPIVersion(){
