81d59fe50e2e6d1b2d71835011bf934cde910a0e
JHRunning <jeonghunn1@gmail.com>
2015-01-25 13:02:04 +0900

    getmypage updated

--- core/api.class.php
+++ core/api.class.php
@@ -23,7 +23,7 @@ echo getCoreVersion();
 
 function API_getMyPageInfo($user_srl){
 	$PAGE = new PageClass();
-	require_once 'core/status.php';
+	//require_once 'core/status.php';
 
 $page_info = REQUEST('page_info');
 //Update new member information
@@ -36,7 +36,7 @@ $page_info = REQUEST('page_info');
 
 function API_getPageInfo($user_srl){
 	$PAGE = new PageClass();
-	require_once 'core/status.php';
+	//require_once 'core/status.php';
 
 $page_srl = REQUEST('page_srl');
 $page_info = REQUEST('page_info');
