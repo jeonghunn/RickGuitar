40b137d8ebe1858c225b9ae9b349ddc00bbff096
JHRunning <jeonghunn1@gmail.com>
2015-02-22 02:03:32 +0900

    permission allow improved

--- core/base.php
+++ core/base.php
@@ -1,7 +1,7 @@
 <?php
 
 function getCoreVersion(){
-    return "2.39.32.1.228";
+    return "2.39.32.2.229";
 }
 
 //Basic Info
--- core/permission.php
+++ core/permission.php
@@ -46,7 +46,7 @@ function getIPManageInfo(){
  (int) $user_permission_status = $user_permission['permission'];
 //Permission Check
   if($user_permission['status'] == 5) ErrorMessage("unknown_error");
-        if($permission_allow == null) $permission_allow = 3;
+        $permission_allow = 3;
         if($user_permission['permission'] > $permission_allow) ErrorMessage("permission_error");
    }
 
