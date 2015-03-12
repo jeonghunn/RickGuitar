e88666bee9d889299284a8e3c06f379384f1d397
JHRunning <jeonghunn1@gmail.com>
2015-03-12 10:40:00 +0900

    test

--- modules/board/documents.class.php
+++ modules/board/documents.class.php
@@ -16,6 +16,10 @@ class DocumentClass
 
         $row['you_doc_status'] = $status;
         $row['attach_contents'] = $ATTACH_CLASS -> attach_read($PAGE_CLASS, $this, $user_srl, $doc_srl);
+
+        if($row['attach_contents'] != null){
+            echo 'akdfj';
+        }
        // $row['me_doc_status'] = $this -> getDocStatus($PAGE_CLASS, $doc_srl, $user_srl);
 
 
