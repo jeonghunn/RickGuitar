70656aef9d6953242db8a673eececcecbb595ae1
JHRunning <jeonghunn1@gmail.com>
2017-01-04 22:00:16 +0900

    script added

--- pages/header.php
+++ pages/header.php
@@ -24,6 +24,52 @@
       <link rel="import" href="pages/bower_components/paper-icon-button/paper-icon-button.html">
 
 
+      <script>
+          // Setup Polymer options
+          window.Polymer = {
+              dom: 'shadow',
+              lazyRegister: true,
+          };
+
+          // Load webcomponentsjs polyfill if browser does not support native
+          // Web Components
+          (function() {
+              'use strict';
+
+              var onload = function() {
+                  // For native Imports, manually fire WebComponentsReady so user code
+                  // can use the same code path for native and polyfill'd imports.
+                  if (!window.HTMLImports) {
+                      document.dispatchEvent(
+                          new CustomEvent('WebComponentsReady', {bubbles: true})
+                      );
+                  }
+              };
+
+              var webComponentsSupported = (
+                  'registerElement' in document
+                  && 'import' in document.createElement('link')
+                  && 'content' in document.createElement('template')
+              );
+
+              if (!webComponentsSupported) {
+                  var script = document.createElement('script');
+                  script.async = true;
+                  script.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
+                  script.onload = onload;
+                  document.head.appendChild(script);
+              } else {
+                  onload();
+              }
+          })();
+
+          // Load pre-caching Service Worker
+    /*      if ('serviceWorker' in navigator) {
+              window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/service-worker.js');
+              });
+          }*/
+      </script>
     <!-- Bootstrap core CSS -->
 <!--    <link href="pages/css/bootstrap.css" rel="stylesheet">-->
 
