ab5ba14fdf06656c1a7e8f40282fec6f4049b3d8
JHRunning <jeonghunn1@gmail.com>
2017-01-04 22:12:12 +0900

    updated

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,5 +1,5 @@
-<!-- Fixed navbar -->
-<paper-toolbar class="medium-tall">
+
+<paper-toolbar>
     <paper-icon-button icon="menu"></paper-icon-button>
     <span class="title">Square</span>
     <paper-icon-button icon="refresh"></paper-icon-button>
