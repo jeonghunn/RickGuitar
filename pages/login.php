1875d86defc970095e744b66fb09ee5830934a26
JHRunning <jeonghunn1@gmail.com>
2017-01-04 21:24:15 +0900

    로그인 페이지 문제 수정

--- pages/login.php
+++ pages/login.php
@@ -3,14 +3,14 @@
 
     <div class="container">
 
-      <form class="form-signin" role="form" method='post' action= '<? echo getClientUrl(true)."?a=loginact'"; ?>>
-        <h2 class="form-signin-heading"><? S('sign_in'); ?></h2>
-        <input type="text" name='id' class="form-control" placeholder="<? S('id');?>" required autofocus>
-        <input type="password" name='password'  class="form-control" placeholder="<? S('password');?>" required>
+      <form class="form-signin" role="form" method='post' action= '<?php echo getClientUrl(true)."?a=loginact'"; ?>>
+        <h2 class="form-signin-heading"><?php S('sign_in'); ?></h2>
+        <input type="text" name='id' class="form-control" placeholder="<?php S('id');?>" required autofocus>
+        <input type="password" name='password'  class="form-control" placeholder="<?php S('password');?>" required>
         <label class="checkbox">
-          <input type="checkbox" value="remember-me"> <? S('remember_me');?>
+          <input type="checkbox" value="remember-me"> <?php S('remember_me');?>
         </label>
-        <button class="btn btn-lg btn btn-block btn-success" type="submit"><? S('sign_in');?></button>
+        <button class="btn btn-lg btn btn-block btn-success" type="submit"><?php S('sign_in');?></button>
       </form>
 
       <br><br>
