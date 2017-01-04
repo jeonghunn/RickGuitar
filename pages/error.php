2ed2539b4e69bfb23c86ca694d4553436d693fcd
JHRunning <jeonghunn1@gmail.com>
2017-01-04 21:35:46 +0900

    error page bug fixed

--- pages/error.php
+++ pages/error.php
@@ -5,11 +5,11 @@
 <!-- html -->
     <div class="container">
 
-    <h1 style="font-size: 63px;text-align: center;color: #fd9800;"><? S('error_'.$error_code.'_error')?></h1>
+    <h1 style="font-size: 63px;text-align: center;color: #fd9800;"><?php S('error_'.$error_code.'_error')?></h1>
 <br><br>
       <center><h3 stlye="font-size: 21px;"><? S('error_'.$error_code.'_error_des')?></h3>
 <div style="margin-top: 5%; margin-left: 30%; margin-right:30%;">
-<img src="pages/images/<? echo $error_code; ?>_error.png" width="70%"></img>
+<img src="pages/images/<?php echo $error_code; ?>_error.png" width="70%"></img>
   </div>
 </center>
     </div> <!-- /container -->
