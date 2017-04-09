d18002b3cfbacbb4f4866bfe642a2a31012132a5
JHRunning <jeonghunn1@gmail.com>
2017-04-09 22:01:45 +0900

    info updated

--- pages/info.php
+++ pages/info.php
@@ -4,7 +4,7 @@
 
   <center>  <h1 style="font-size: 36px; center;">SquareCore</h1>
 <br><br>
-      <h3 style="font-size: 21px;" ><? echo PostAct(getAPIUrl(),  array(array('api_key', getAPIKey()), array('a', 'CoreVersion'))); ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
+      <h3 style="font-size: 21px;" ><?php echo PostAct(getAPIUrl(),  array(array('api_key', getAPIKey()), array('a', 'CoreVersion'))); ?></h3><br><br><h3 stlye="font-size: 18px;">Developed By Junghoon Lee</h3>
 
 
   </div>
