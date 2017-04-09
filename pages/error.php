b96dc85da8511d23d1275d3b98971fa3ef74ea66
JHRunning <jeonghunn1@gmail.com>
2017-04-09 20:08:50 +0900

    404 not found error test

--- pages/error.php
+++ pages/error.php
@@ -1,7 +1,7 @@
 
 
 
-<?php   header('HTTP/1.0 404 Not Found', true, 404); ?>
+<?php  if($error_code == 404)   header('HTTP/1.0 404 Not Found'); ?>
 <!-- html -->
     <div class="container">
 
@@ -14,3 +14,5 @@
 </center>
     </div> <!-- /container -->
 
+
+<?php  exit(); ?>
