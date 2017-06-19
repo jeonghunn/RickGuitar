61ff2f80902b3987c9c069f9254749210f2387ff
JHRunning <jeonghunn1@gmail.com>
2017-06-19 22:16:38 +0900

    card added

--- pages/header.php
+++ pages/header.php
@@ -86,6 +86,17 @@
           app-header paper-icon-button {
               --paper-icon-button-ink-color: white;
           }
+         .card {
+             /* Add shadows to create the "card" effect */
+             box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+             transition: 0.3s;
+             border-radius: 8px; /* 5px rounded corners */
+         }
+
+         /* On mouse-over, add a deeper shadow */
+         .card:hover {
+             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+         }
 
 
       </style>
