ddda732c1b155e04b0002421a6f5bdb8e8b248a0
JHRunning <jeonghunn1@gmail.com>
2017-06-19 22:18:23 +0900

    set color

--- pages/header.php
+++ pages/header.php
@@ -88,6 +88,7 @@
           }
          .card {
              /* Add shadows to create the "card" effect */
+             background-color: #FFFFFF;
              box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
              transition: 0.3s;
              border-radius: 8px; /* 5px rounded corners */
