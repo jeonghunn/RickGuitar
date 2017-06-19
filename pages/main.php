f3e8ad9dbb28d1171c975c78b3bb22fa6bce68dd
JHRunning <jeonghunn1@gmail.com>
2017-06-19 22:38:38 +0900

    squarecard updated

--- pages/main.php
+++ pages/main.php
@@ -12,6 +12,10 @@
                  <h4>축하해줄 사람의 이름, 생일 그리고 할 말을 작성해주세요.</h4>
 <hr>
 
+              <div class="input-group input-group-lg">
+                  <input type="text" class="form-control" placeholder="이름" aria-describedby="sizing-addon1">
+              </div>
+
           </div>
 
 </center><br><br><br>
