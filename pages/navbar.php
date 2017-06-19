b98ad3c1d736d4da065f0de5af38114ae05fbe9d
JHRunning <jeonghunn1@gmail.com>
2017-06-19 22:09:41 +0900

    navbar updated

--- pages/navbar.php
+++ pages/navbar.php
@@ -11,11 +11,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#"><?php S('app_name')?></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
                 <li class="dropdown">
