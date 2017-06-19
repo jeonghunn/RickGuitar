3facf2d232c325be698aa9f50b258d89095f7dc3
JHRunning <jeonghunn1@gmail.com>
2017-06-19 22:25:09 +0900

    squarecard updated

--- pages/main.php
+++ pages/main.php
@@ -6,7 +6,7 @@
     <h1 style="font-size: 63px;text-align: center;color: <?php  getTitleColor() ?>;">참신하게 생일을 축하해보세요.</h1>
 <br><br>
       <center><h3 stlye="font-size: 21px;">그 누구도 예상하지못한 생일축하방법</h3>
-
+<br>
           <div class="squarecard">
 
                   This is some text within a card block.
