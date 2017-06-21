7ae0350c172a4b7ce7819ad0f22afcbc66ea7c9f
JHRunning <jeonghunn1@gmail.com>
2017-06-22 00:53:43 +0900

    ip manage system updated

--- config.php
+++ config.php
@@ -28,14 +28,16 @@ if($user_srl == null) $user_srl = 0;
 require_once 'core/logger.php';
 require_once 'core/security.php';
 require_once 'core/permission.php';
+
+//Check IP
+PermissionCheckAct($user_srl);
+IPManageAct(getIPAddr(), getNowUrl(), getTimeStamp());
+
 //Log Client
 ActLogSyncTask($user_srl, getIPAddr(),getTimeStamp(), $log_category, $log);
 ClientAgentLogSyncTask($user_srl);
 
 //set user_Srl
 
-//Check IP
-PermissionCheckAct($user_srl);
-IPManageAct(getIPAddr(), getNowUrl(), getTimeStamp());
 
 ?>
