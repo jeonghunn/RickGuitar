a9522c20fd335720cc22b36dd987c8851e6219a8
JHRunning <jeonghunn1@gmail.com>
2017-06-22 02:34:25 +0900

    page square updated

--- pages/header.php
+++ pages/header.php
@@ -98,15 +98,7 @@
          }
 
          .centered {
-             width: 100%;
-             height: 100%;
-             position: absolute;
-             top: 0;
-             bottom: 0;
-             left: 0;
-             right: 0;
-
-             margin: auto;
+             top: 50%;
 
          }
 
