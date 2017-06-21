64591eaeba4693119b5ca8b1796a635ae8b5c66e
JHRunning <jeonghunn1@gmail.com>
2017-06-22 02:40:15 +0900

    page square updated

--- pages/square.php
+++ pages/square.php
@@ -4,9 +4,10 @@
     <div class="squarecard">
         <div class="centered">
             <br><br><br><br><br><br>
-        <p><span style="font-size: 24px;"><span style="font-size: 32px;">이진오</span> 님은 <br><span
-                        style="font-size: 32px;">1998</span>년 <span
-                        style="font-size: 32px;">10</span>월 <span style="font-size: 32px;">17</span>일에<br> 태어났고,</span>
+            <p><span style="font-size: 32px;"><span style="font-size: 40px;">이진오</span> 님은 <br><span
+                            style="font-size: 40px;">1998</span>년 <span
+                            style="font-size: 40px;">10</span>월 <span
+                            style="font-size: 40px;">17</span>일에<br> 태어났고,</span>
         </p>
 
         </div>
