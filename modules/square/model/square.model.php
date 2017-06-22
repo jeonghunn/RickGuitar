731b8e7058cf5654a1052b13bd43b8a8bf61b5e9
JHRunning <jeonghunn1@gmail.com>
2017-06-23 00:57:54 +0900

    write method updated

--- modules/square/model/square.model.php
+++ modules/square/model/square.model.php
@@ -9,7 +9,7 @@ function Model_Square_getLastNumber()
 
 function Model_Square_Write($square_key, $page_srl, $user_srl, $name, $title, $content, $type, $data, $permission, $status, $privacy, $attach_result)
 {
-    return DBQuery("INSERT INTO `square` (`square_key`, `page_srl`, `user_srl`, `name`, `title`, `content`, `data`, `date`, `permission`, `status`, `privacy`,  `attach`,  `ip_addr`) VALUES ('$square_key', '$page_srl', '$user_srl', '$name', '$title', '$content', '$type', '$data',  '" . getTimeStamp() . "', '$permission', '$status', '$privacy', '$attach_result ? 1 : 0', '" . getIPAddr() . "');");
+    return DBQuery("INSERT INTO `square` (`square_key`, `page_srl`, `user_srl`, `name`, `title`, `content`,`type`, ,`data`, `date`, `permission`, `status`, `privacy`,  `attach`,  `ip_addr`) VALUES ('$square_key', '$page_srl', '$user_srl', '$name', '$title', '$content', '$type', '$data',  '" . getTimeStamp() . "', '$permission', '$status', '$privacy', '$attach_result ? 1 : 0', '" . getIPAddr() . "');");
 }
 
 
