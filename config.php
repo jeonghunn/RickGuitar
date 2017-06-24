a118c3173fd9a24afc08def9f7d549bf2a669743
JHRunning <jeonghunn1@gmail.com>
2017-06-25 03:56:59 +0900

    client log bug fixed

--- config.php
+++ config.php
@@ -40,6 +40,7 @@ $ipmanage = IPManageAct(getIPAddr(), getNowUrl(), getTimeStamp());
 //Log Client
 if ($ipmanage) {
     ActLog($user_srl, getIPAddr(), getTimeStamp(), $log_category, $log);
+    ClientAgentLog($user_srl, getIPAddr(), getUserAgent(), getTimeStamp());
 }
 
 
--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.0.623.17";
+    return "3.0.625";
 }
 
 
