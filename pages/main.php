87fb9281b02f505fdbb73d5f71ca44eda7055128
JHRunning <jeonghunn1@gmail.com>
2017-06-28 03:33:32 +0900

    error catch

--- pages/main.php
+++ pages/main.php
@@ -105,7 +105,7 @@
                 }
             },
 
-            fail: function (jqXHR) {
+            error: function (jqXHR) {
                 alert('죄송합니다. 뭔가 문제가 있는거 같아요. 잠시 후에 다시 시도해주세요 ㅠㅠ 문의 : jeonghunn1@gmail.com');
             }
 
