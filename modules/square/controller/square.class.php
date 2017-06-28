c5e3be48d049020e92695c44e2c17e8ec358932c
JHRunning <jeonghunn1@gmail.com>
2017-06-28 22:23:29 +0900

    test

--- modules/square/controller/square.class.php
+++ modules/square/controller/square.class.php
@@ -135,9 +135,8 @@ class SquareClass
         if ($WriteResult != null) {
             $title = $WriteResult[0];
             $content = $WriteResult[1];
-
-            //print_r($WriteResult[2]);
             $data = json_encode($WriteResult[2]);
+            echo $data;
             $square_cards = json_encode($WriteResult[3]);
 
         }
