64eecb779f57792a4517ca6cb44ff88c94e30460
JHRunning <jeonghunn1@gmail.com>
2017-07-01 03:05:07 +0900

    bug fixed

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ LoadPages("error", "error", false);
 LoadPages("info", "info", false);
 LoadPages("infodetail", "infodetail", false);
 LoadPages("signup", "signup", false);
-LoadPages("birthday", "birthday/brithday_write", false);
+LoadPages("birthday", "birthday/birthday_write", false);
 //API
 	    LoadPages("api_main", "api/api_main", false);
 		LoadPages("api_add", "api/api_add", true);
