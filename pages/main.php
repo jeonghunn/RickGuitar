a2f7bacb7e8d7fbf3333ba1a2dd5a72b2dc090f0
JHRunning <jeonghunn1@gmail.com>
2017-07-01 23:31:59 +0900

    squarecard updated

--- pages/main.php
+++ pages/main.php
@@ -42,7 +42,7 @@
 
         // add the newly created element and its content int
 
-        var currentDiv = document.getElementById("div1");
+        var currentDiv = document.getElementById("firstcard");
         document.body.insertBefore(newDiv, currentDiv);
     }
 
