857597e7e500bb79f1f3556d8e428a1331953863
JHRunning <jeonghunn1@gmail.com>
2017-07-01 23:14:07 +0900

    test

--- pages/main.php
+++ pages/main.php
@@ -107,12 +107,6 @@
     }
 
 
-    function addCard() {
-        var btn = document.createElement("BUTTON");
-        var t = document.createTextNode("CLICK ME");
-        btn.appendChild(t);
-        document.body.appendChild(btn);
-    }
 
     function setbuttonstatus(status) {
 
