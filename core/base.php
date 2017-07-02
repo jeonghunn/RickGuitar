7e895e8b87b313f9013f375236e904f06c2876c2
JHRunning <jeonghunn1@gmail.com>
2017-07-02 17:03:03 +0900

    db input bug fixed

--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.1.702.17";
+    return "3.1.702.17.2";
 }
 
 
--- modules/square/model/square_card.model.php
+++ modules/square/model/square_card.model.php
@@ -9,7 +9,7 @@ function Model_SquareCards_getLastNumber()
 
 function Model_SquareCards_Write($page_srl, $user_srl, $square_srl, $parent_card_srl, $name, $content, $permission, $status, $privacy, $attach_result)
 {
-    return DBQuery("INSERT INTO `square`  ( `page_srl`, `user_srl`,  `square_srl`,  `parent_card_srl`,  `name`,`content`, `date`, `permission`, `status`, `privacy`,  `attach`,  `ip_addr`) VALUES ('$page_srl', '$user_srl', '$square_srl', '$parent_card_srl', '$name',  '$content', '" . getTimeStamp() . "', '$permission', '$status', '$privacy', '$attach_result ? 1 : 0', '" . getIPAddr() . "');");
+    return DBQuery("INSERT INTO `square_cards`  ( `page_srl`, `user_srl`,  `square_srl`,  `parent_card_srl`,  `name`,`content`, `date`, `permission`, `status`, `privacy`,  `attach`,  `ip_addr`) VALUES ('$page_srl', '$user_srl', '$square_srl', '$parent_card_srl', '$name',  '$content', '" . getTimeStamp() . "', '$permission', '$status', '$privacy', '$attach_result ? 1 : 0', '" . getIPAddr() . "');");
 }
 
 //function Model_Square_getSquareByKey($square_key)
