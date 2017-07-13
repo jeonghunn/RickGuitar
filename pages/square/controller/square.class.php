56e327fbcd334022354188bc93310be25af86c72
JHRunning <jeonghunn1@gmail.com>
2017-07-14 05:17:32 +0900

    tag updated

--- pages/square/controller/square.class.php
+++ pages/square/controller/square.class.php
@@ -17,11 +17,13 @@ class SquareClass
         $contents = str_replace("{[/", "</", $contents);
         $contents = str_replace("{[br", "<br", $contents);
         $contents = str_replace("{[span", "<span", $contents);
-        $contents = str_replace("{[img", "<img", $contents);
         $contents = str_replace("{[h", "<h", $contents);
         $contents = str_replace("{[p", "<p", $contents);
         $contents = str_replace("{[b", "<b", $contents);
         $contents = str_replace("{[i", "<i", $contents);
+        $contents = str_replace("{[u", "<u", $contents);
+        $contents = str_replace("{[o", "<o", $contents);
+        $contents = str_replace("{[t", "<t", $contents);
         $contents = str_replace("&quot;", '"', $contents);
         $contents = str_replace('\"', '"', $contents);
         $contents = str_replace('\'', "''", $contents);
