625f7eb317ff97254a963d3bb3c219a9df4595e4
JHRunning <jeonghunn1@gmail.com>
2017-07-14 05:02:28 +0900

    birthday card updated

--- modules/square/controller/birthday.class.php
+++ modules/square/controller/birthday.class.php
@@ -31,7 +31,7 @@ class BirthdayClass
         $birthday_contents = $data['birthday_contents'];
         $birthday_wiki = $data['birthday_wiki'];
         $birthday_wiki_contents = '{[br]}{[h4]}' . $birthday_month . '월 ' . $birthday_day . '일에는 무슨 일들이 일어났을까요?{[/h4]}
-                {[hr]}{[span style="font-size: 12px;"]}' . $this->getBirthdayWikiContents($birthday_wiki, $birthday_month, $birthday_day) . "{[/span]}";
+                {[hr]}{[span style="font-size: 14px;"]}' . $this->getBirthdayWikiContents($birthday_wiki, $birthday_month, $birthday_day) . "{[/span]}";
 
         $result_array = array(array("content" => "{[span style=\"font-size: 100px;\"]}동현{[/span]}"),
             array("content" => '{[p]}{[span style="font-size: 32px;"]}{[span
