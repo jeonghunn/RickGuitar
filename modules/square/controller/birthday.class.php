2277fb01d9310ffdec34f1138de459fe50e9ab08
JHRunning <jeonghunn1@gmail.com>
2017-07-14 05:07:45 +0900

    test

--- modules/square/controller/birthday.class.php
+++ modules/square/controller/birthday.class.php
@@ -31,7 +31,7 @@ class BirthdayClass
         $birthday_contents = $data['birthday_contents'];
         $birthday_wiki = $data['birthday_wiki'];
         $birthday_wiki_contents = '{[br]}{[h4]}' . $birthday_month . '월 ' . $birthday_day . '일에는 무슨 일들이 일어났을까요?{[/h4]}
-                {[hr]}{[p]}' . $this->getBirthdayWikiContents($birthday_wiki, $birthday_month, $birthday_day) . "{[/p]}";
+                {[hr]}{[p style="font-size: 14px;"]}' . $this->getBirthdayWikiContents($birthday_wiki, $birthday_month, $birthday_day) . "{[/p]}";
 
         $result_array = array(array("content" => "{[span style=\"font-size: 100px;\"]}동현{[/span]}"),
             array("content" => '{[p]}{[span style="font-size: 32px;"]}{[span
