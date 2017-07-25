e92169b06be61dd6964a3ab8485e1bcae940576c
JHRunning <jeonghunn1@gmail.com>
2017-07-25 23:51:12 +0900

    checked updated

--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.4.725.23.45";
+    return "3.4.725.23.50";
 }
 
 
--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -12,7 +12,8 @@
 
                         <textarea class="cardtextarea" placeholder="내용을 입력해주세요." id="contents_1"></textarea>
                         <label class="radio-inline">
-                            <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 가운데 정렬
+                            <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked> 가운데
+                            정렬
                         </label>
                         <label class="radio-inline">
                             <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 왼쪽 정렬
