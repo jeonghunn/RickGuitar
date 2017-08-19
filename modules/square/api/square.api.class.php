ed2701db8a5734305216cef220d7b6072594bbea
JHRunning <jeonghunn1@gmail.com>
2017-08-20 05:17:15 +0900

    getReadResult style added

--- modules/square/api/square.api.class.php
+++ modules/square/api/square.api.class.php
@@ -38,7 +38,7 @@ class SquareApiClass{
         //  $square_i]nfo = REQUEST('doc_info');
         $attach_info = REQUEST('attach_info');
 
-        $square_info = array('square_key', 'page_srl', 'user_srl', 'name', 'title', 'content', 'align', 'type', 'data', 'date', 'status', 'attach', 'square_cards');
+        $square_info = array('square_key', 'page_srl', 'user_srl', 'name', 'title', 'content', 'type', 'data', 'date', 'status', 'attach', 'square_cards');
 
         $square_read = $SQUARE_CLASS->Read($SQUARE_CARD_CLASS, $PAGE_CLASS, $ATTACH_CLASS, $user_srl, $square_key, ExplodeInfoValue($attach_info));
         print_info($square_read, $square_info);
