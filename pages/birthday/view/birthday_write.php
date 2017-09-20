4cb5e3a5c28353be4f1f7a993a46229727f48671
JHRunning <jeonghunn1@gmail.com>
2017-09-20 15:52:48 +0900

    birthday message updated

--- pages/birthday/view/birthday_write.php
+++ pages/birthday/view/birthday_write.php
@@ -7,8 +7,7 @@ require_once 'pages/header.php'; ?>
 
 <!-- html -->
 <div class="container">
-
-    <h1 style="font-size: 63px;text-align: center;color: <?php getTitleColor() ?>;">참신하게 생일을 축하해보세요.</h1>
+    <h1 style="text-align: center;color: <?php getTitleColor() ?>;">참신하게 생일을 축하해보세요.</h1>
     <br><br>
     <center><h3 stlye="font-size: 21px;">그 누구도 예상하지 못한 생일축하방법</h3>
         <br>
