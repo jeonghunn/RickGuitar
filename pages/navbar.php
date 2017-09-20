c80396e180a4f1bb43191868af3ef31568d2d944
JHRunning <jeonghunn1@gmail.com>
2017-09-20 15:56:16 +0900

    title updated

--- pages/navbar.php
+++ pages/navbar.php
@@ -11,7 +11,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <b><a class="navbar-brand" href="home" style="font-size: 24px;"><?php S('app_full_name') ?></a></b>
+            <b><a class="navbar-brand" href="home" style="font-size: 18px;"><?php S('app_full_name') ?></a></b>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
