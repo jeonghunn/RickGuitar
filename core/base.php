7898684f8a23166a824f7dc781cb437a97e7cd39
JHRunning <jeonghunn1@gmail.com>
2017-11-09 23:32:03 +0900

    info updated

--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.5.1108";
+    return "3.5.1109";
 }
 
 //Basic Info
--- pages/footer.php
+++ pages/footer.php
@@ -1,9 +1,10 @@
 <div id="footer" style="margin-top: 80px;">
 
-    <div class="container">
+    <a class="container">
         <br>
         <hr>
-        <p class="text-muted" style="text-align: center;" href="info">Copyrightⓒ. 2017. Tarks. All Rights Reserved.</p>
+        <a href="info"></a>
+        <p class="text-muted" style="text-align: center;">Copyrightⓒ. 2017. Tarks. All Rights Reserved.</p></a>
     </div>
 
 </div>
