5d76d52adc09f1b33253502cd1ba6c57e3614129
Junghoon Lee <jeonghunn1@gmail.com>
2017-12-26 19:06:20 +0900

    blur updated

--- pages/header.php
+++ pages/header.php
@@ -218,7 +218,7 @@
               top: 0;
               left: 0;
               width: 100%;
-              height: 5rem;
+              height: 100%;
               overflow: hidden;
               position: fixed;
               filter: blur(4px);
