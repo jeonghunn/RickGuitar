b0d0d794010fd380e52a12b6020fda9c4cd01f65
Junghoon Lee <jeonghunn1@gmail.com>
2017-12-27 14:53:54 +0900

    btn group large

--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.6.1227.2";
+    return "3.6.1227.14";
 }
 
 //Basic Info
--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -31,18 +31,18 @@
             <!--            버튼 툴바-->
             <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups"
                  style="  text-align: center;">
-                <div class="btn-group" role="group" aria-label="First group">
+                <div class="btn-group btn-group-lg" role="group" aria-label="First group">
                     <button type="button" class="btn btn-default">1</button>
                     <button type="button" class="btn btn-default">2</button>
                     <button type="button" class="btn btn-default">3</button>
                     <button type="button" class="btn btn-default">4</button>
                 </div>
-                <div class="btn-group" role="group" aria-label="Second group">
+                <div class="btn-group btn-group-lg" role="group" aria-label="Second group">
                     <button type="button" class="btn btn-default">5</button>
                     <button type="button" class="btn btn-default">6</button>
                     <button type="button" class="btn btn-default">7</button>
                 </div>
-                <div class="btn-group" role="group" aria-label="Third group">
+                <div class="btn-group btn-group-lg" role="group" aria-label="Third group">
                     <button type="button" class="btn btn-default">8</button>
                 </div>
             </div>
