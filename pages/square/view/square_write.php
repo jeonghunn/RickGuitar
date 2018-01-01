aa90411ed89312bd1d0aab298129f9ed01513c9b
JHRunning <jeonghunn1@gmail.com>
2018-01-01 21:16:47 +0900

    test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -79,9 +79,8 @@
 
     var editor = new MediumEditor('.squarecard', {
         toolbar: {
-            buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'image', 'justifyLeft', 'justifyCenter', 'justifyRight', 'orderedlist', 'unorderedlist', 'pre'],
+            buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'image', 'justifyLeft', 'justifyCenter', 'justifyRight', 'orderedlist', 'unorderedlist', 'pre', 'h1'],
         },
-        buttonLabels: 'fontawesome',
         anchor: {
             targetCheckbox: true
         }
