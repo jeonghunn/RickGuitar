55224bf5bb46968065e58dbdaf5b41719aa72c32
JHRunning <jeonghunn1@gmail.com>
2018-01-05 01:07:24 +0900

    covertforread updated

--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.7.105.0.41";
+    return "3.7.105.1";
 }
 
 //Basic Info
--- pages/lib/lib.loader.php
+++ pages/lib/lib.loader.php
@@ -13,6 +13,7 @@ require_once 'pages/lib/HTMLPurifier/HTMLPurifier.auto.php';
 
 //load lib settings
 $HTML_PURIFIER_CONFIG = HTMLPurifier_Config::createDefault();
+$HTML_PURIFIER_CONFIG->set('HTML.Allowed', 'u,p,b,i,span[style],p,strong,em,li,ul,ol,div[align],br,img');
 $HTML_PURIFIER_CONFIG->set('HTML.AllowedAttributes', 'src, height, width, alt');
 
 //load apis
