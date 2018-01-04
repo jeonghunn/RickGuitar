a94269f6fd4586762aefb9a64ee8ede15618eaed
JHRunning <jeonghunn1@gmail.com>
2018-01-05 01:03:04 +0900

    lib setting updated

--- pages/lib/lib.loader.php
+++ pages/lib/lib.loader.php
@@ -13,7 +13,7 @@ require_once 'pages/lib/HTMLPurifier/HTMLPurifier.auto.php';
 
 //load lib settings
 $HTML_PURIFIER_CONFIG = HTMLPurifier_Config::createDefault();
-
+$HTML_PURIFIER_CONFIG->set('HTML.AllowedAttributes', 'src, height, width, alt');
 
 //load apis
 $HTML_PURIFIER = new HTMLPurifier($HTML_PURIFIER_CONFIG);
