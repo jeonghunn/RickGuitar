e01c865ee3b3cec8d3a9a560662afe1d2759b16a
JHRunning <jeonghunn1@gmail.com>
2018-01-21 02:25:30 +0900

    main focus updated

--- pages/square/view/square_main.php
+++ pages/square/view/square_main.php
@@ -7,7 +7,7 @@
 <div class="container">
 
 
-    <div class="squarecardw" onfocus="location.href='write'; " contenteditable="true">새로운 카드 만들기</div>
+    <div class="squarecardw" onclick="location.href='write'; " contenteditable="true">새로운 카드 만들기</div>
 
 </div> <!-- /container -->
 
