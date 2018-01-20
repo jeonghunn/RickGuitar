1ea5f1163326c3f0ab3f93efb291c6dbc6286aaf
JHRunning <jeonghunn1@gmail.com>
2018-01-21 02:35:05 +0900

    mainbig

--- core/base.php
+++ core/base.php
@@ -2,7 +2,7 @@
 
 
 function getCoreVersion(){
-    return "3.8.121.2.30";
+    return "3.8.121.2.32";
 }
 
 //Basic Info
--- pages/square/view/square_main.php
+++ pages/square/view/square_main.php
@@ -8,7 +8,7 @@
 
 
     <div class="squarecardw" onclick="location.href='write'; " contenteditable="true">새로운 카드 만들기</div>
-
+    <br>
     <div class="squarecard_mainbig">Big 1</div>
     <div class="squarecard_mainbig">Big 2</div>
     <div class="squarecard_mainbig">Big 3</div>
