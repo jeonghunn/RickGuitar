0c8092d278d479dc57d2b62897c9b0671656f015
JHRunning <jeonghunn1@gmail.com>
2018-01-21 03:19:39 +0900

    main layout

--- pages/square/view/square_main.php
+++ pages/square/view/square_main.php
@@ -71,11 +71,50 @@
         </div>
 
 
-
+        <br>
 
     </div>
 
 
-    </div> <!-- /container -->
+        <div id="group">
+            <h1>새로운 업데이트</h1>
+            <br>
+            <div class="row">
+                <div class="col-sm-6 col-md-3">
+
+                    <div class="squarecard_mainbig">Big 1</div>
+
+
+                </div>
+
+                <div class="col-sm-6 col-md-3">
+
+
+                    <div class="squarecard_mainbig">Big 2</div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-3">
+
+
+                    <div class="squarecard_mainbig">Big 2</div>
+
+                </div>
+
+
+                <div class="col-sm-6 col-md-3">
+
+
+                    <div class="squarecard_mainbig">Big 2</div>
+
+                </div>
+
+
+                <br>
+
+            </div>
+
+
+        </div> <!-- /container -->
 
 
