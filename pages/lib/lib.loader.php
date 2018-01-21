bebeee883e49a41476328e0f69e52f79e330f210
Junghoon Lee <jeonghunn1@gmail.com>
2018-01-21 16:52:33 +0900

    test

--- pages/lib/lib.loader.php
+++ pages/lib/lib.loader.php
@@ -14,7 +14,7 @@ require_once 'pages/lib/HTMLPurifier/HTMLPurifier.auto.php';
 //load lib settings
 $HTML_PURIFIER_CONFIG = HTMLPurifier_Config::createDefault();
 $HTML_PURIFIER_CONFIG->set('URI.AllowedSchemes', array('data' => true));
-$HTML_PURIFIER_CONFIG->set('HTML.Allowed', 'a[href],u,p,b,i,span[style],p,strong,em,li,ul,ol,div[align],br,img');
+//$HTML_PURIFIER_CONFIG->set('HTML.Allowed', 'a[href],u,p,b,i,span[style],p,strong,em,li,ul,ol,div[align],br,img');
 $HTML_PURIFIER_CONFIG->set('HTML.AllowedAttributes', 'href, src, height, width, alt');
 
 //load apis
