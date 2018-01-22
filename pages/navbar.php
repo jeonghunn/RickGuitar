e8289fcb81b4781420a4d87f4d392a1f4dedd963
JHRunning <jeonghunn1@gmail.com>
2018-01-23 03:23:52 +0900

    navbar test

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,6 +1,6 @@
 <div id="blurryscroll" aria-hidden="true"></div>
 <!-- Static navbar -->
-<nav class="navbar navbar-light bg-light justify-content-between">
+<nav class="navbar sticky-top navbar-light bg-light">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
