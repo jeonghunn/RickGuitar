74235c08934ca88c19a64f3421290fa258689c4c
JHRunning <jeonghunn1@gmail.com>
2018-01-23 03:35:56 +0900

    navbar test

--- pages/navbar.php
+++ pages/navbar.php
@@ -7,7 +7,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav mr-auto">
 
             <li class="nav-item">
                 <a class="nav-link" href="birthday"><?php S('birthday') ?></a>
