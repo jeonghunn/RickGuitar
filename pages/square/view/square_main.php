0431eb63f3a58aa00089ec09997f49837b386c13
JHRunning <jeonghunn1@gmail.com>
2018-01-28 23:12:28 +0900

    main card test

--- pages/square/view/square_main.php
+++ pages/square/view/square_main.php
@@ -77,10 +77,9 @@
         <div id="group" class="card-deck">
             <h2>새로운 업데이트</h2>
             <br>
-            <div class="row">
-                <div class="col-sm-6 col-md-3">
 
-                    <div class="squarecard_mainbig">
+
+            <div class="squarecard_mainbig">
                         <p>Hi There!</p>
 
                             <small class="text-muted">Last updated 3 mins ago</small>
@@ -88,32 +87,20 @@
                     </div>
 
 
-                </div>
+                    <div class="squarecard_mainbig">Big 2</div>
 
-                <div class="col-sm-6 col-md-3">
+
 
 
                     <div class="squarecard_mainbig">Big 2</div>
 
-                </div>
 
-                <div class="col-sm-6 col-md-3">
 
 
                     <div class="squarecard_mainbig">Big 2</div>
 
-                </div>
 
-
-                <div class="col-sm-6 col-md-3">
-
-
-                    <div class="squarecard_mainbig">Big 2</div>
-
-                </div>
-
-
-                <br>
+            <br>
 
             </div>
 
