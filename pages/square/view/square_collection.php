18515d4aaf9d20391fa8245c42c0a6f48abd1494
JHRunning <jeonghunn1@gmail.com>
2018-01-29 00:12:21 +0900

    collection updated

--- pages/square/view/square_collection.php
+++ pages/square/view/square_collection.php
@@ -125,15 +125,14 @@
         </div>
 
 
-        <nav aria-label="...">
-            <ul class="pagination">
+        <br><br>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
                 <li class="page-item disabled">
                     <a class="page-link" href="#" tabindex="-1">Previous</a>
                 </li>
                 <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item active">
-                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                </li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
                 <li class="page-item"><a class="page-link" href="#">3</a></li>
                 <li class="page-item">
                     <a class="page-link" href="#">Next</a>
