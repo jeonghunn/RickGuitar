01e6377a17395b25ad7b47fa3d6c91f9a2d78e58
JHRunning <jeonghunn1@gmail.com>
2018-01-29 00:01:33 +0900

    main card test

--- pages/square/view/square_main.php
+++ pages/square/view/square_main.php
@@ -81,9 +81,7 @@
 
 
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                         additional content. This content is a little bit longer.</p>
                     <p class="card-text">
@@ -92,9 +90,7 @@
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional
                         content.</p>
                     <p class="card-text">
@@ -103,9 +99,7 @@
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                         additional content. This card has even longer content than the first to show that equal height
                         action.</p>
