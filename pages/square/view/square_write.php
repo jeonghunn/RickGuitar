96bb7a883b3af7251460f5827b9eacd089ba4c5d
Junghoon Lee <jeonghunn1@gmail.com>
2018-02-05 04:42:20 +0900

    temp save bug fixe

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -112,11 +112,11 @@
     setInterval(function () {
         var editables = document.querySelectorAll('.squarecard');
         localStorage.setItem('temp_card_count', editables.length);
+        document.getElementById("status_messange").innerHTML = "임시 저장되었습니다. " + new Date().toLocaleString();
         for (var i = 0; i < editables.length; i++) {
             localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
 
         }
-        document.getElementById("status_messange").innerHTML = "임시 저장되었습니다. " + new Date().toLocaleString();
     ,
         5000
     );
