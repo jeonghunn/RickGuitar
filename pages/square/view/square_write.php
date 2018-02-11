2c289b8bb93348ce7b0577319b8fcae56f016045
Junghoon Lee <jeonghunn1@gmail.com>
2018-02-11 18:41:19 +0900

    fontcontroll test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -69,6 +69,7 @@
 
             <!--     Size, Font       -->
             <div class="collapse" id="fontcontrol">
+                <br>
                 <div class="card card-body">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
                     Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
