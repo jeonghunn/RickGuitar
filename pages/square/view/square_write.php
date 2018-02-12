b213a59fbd740d27c7118206c7d7614961cc8896
Junghoon Lee <jeonghunn1@gmail.com>
2018-02-12 19:32:44 +0900

    test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -201,6 +201,20 @@
         alert('Selected value: ' + $(this).val());
     });
 
+    $(function () {
+        $(document.body).bind('mouseup', function (e) {
+            var selection;
+
+            if (window.getSelection) {
+                selection = window.getSelection();
+            } else if (document.selection) {
+                selection = document.selection.createRange();
+            }
+
+            selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
+        });
+    });
+
 
     function getTemp() {
 
