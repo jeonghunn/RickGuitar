d85e2c05df757a07cb7eb98cef58a51773c4f38d
Junghoon Lee <jeonghunn1@gmail.com>
2019-01-24 12:13:57 +0900

    url has been updated

--- pages/info/view/info.php
+++ pages/info/view/info.php
@@ -78,8 +78,8 @@
                     <h3>Information</h3>
                     <a href="http://unopenedbox.com/index.php?mid=updates&category=228319" class="btn btn-outline-dark"
                        role="button">Changelog</a>
-                    <a href="http://tarks.net/fsquare_notice" class="btn btn-outline-dark" role="button">Notice</a>
-                    <a href="http://tarks.net/fsquare_feedback" class="btn btn-outline-dark" role="button">FeedBack</a>
+                    <a href="http://unopenedbox.com/notice" class="btn btn-outline-dark" role="button">Notice</a>
+                    <a href="http://unopenedbox.com/fsquare_feedback" class="btn btn-outline-dark" role="button">FeedBack</a>
                     <a href="license" class="btn btn-outline-dark" role="button">Open Source License</a>
 
 
