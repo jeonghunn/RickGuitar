70e13e8e2d88b836d2a619af0a11ebf59ff5c989
JHRunning <jeonghunn1@gmail.com>
2019-03-01 00:40:46 +0900

    navbar write button updated.

--- pages/navbar.php
+++ pages/navbar.php
@@ -21,9 +21,10 @@
 
         <?php if (isDevelopmentServer()) echo "<li class=\"nav-item\"><a  class=\"nav-link\" href=\"info\">Development Server</a></li>" ?>
 
-        <?php if (getActParameter() != "write") echo "<button type=\"nav-button\" class=\"btn btn-dark btn-lg\" onclick=\"location.href='write';  \">+</button>" ?>
+
 
         </ul>
 
+        <?php if (getActParameter() != "write" && getActParameter() != "home") echo "<button type=\"nav-button\" class=\"btn btn-dark btn-lg\" onclick=\"location.href='write';  \">+</button>" ?>
     </div>
 </nav>
