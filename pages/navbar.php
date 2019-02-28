55c8b28cf0d88ae2ced80c762a11c95d05cee82e
JHRunning <jeonghunn1@gmail.com>
2019-03-01 00:21:51 +0900

    write button on navbar : update

--- pages/navbar.php
+++ pages/navbar.php
@@ -19,7 +19,7 @@
         </ul>
         <ul class="navbar-nav">
             <button type="nav-button" class="btn btn-dark btn-lg"
-                    style=" border-radius: 50%; width: 24px; height: 24px; text-align: center; font-size:24px"
+                    style=" border-radius: 50%; width: 32px; height: 32px; text-align: center; font-size:24px"
                     onclick="location.href='write'; ">+
             </button>
         <?php if (isDevelopmentServer()) echo "<li class=\"nav-item\"><a  class=\"nav-link\" href=\"info\">Development Server</a></li>" ?>
