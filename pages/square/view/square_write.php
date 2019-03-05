315b13cddc09472c00789e6e4499814f7029ae7c
JHRunning <jeonghunn1@gmail.com>
2019-03-06 01:12:05 +0900

    font size test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -39,7 +39,7 @@ require_once 'pages/header.php'; ?>
                     <option value="6">6</option>
                     <option value="7">7</option>
                     <option value="8">8</option>
-                    <option value="100px">15</option>
+                    <option value="100">15</option>
 
                 </select>
 
