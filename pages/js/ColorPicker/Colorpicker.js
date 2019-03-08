3704ee464218a7b278f6089d5532f11cd45080b6
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-09 03:38:46 +0900

    test

--- pages/js/ColorPicker/Colorpicker.js
+++ pages/js/ColorPicker/Colorpicker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import Extension from './Extension';
+import {Extension} from './Extension';
 import defaults from './options';
 import coreExtensions from 'extensions';
 import $ from 'jquery';
