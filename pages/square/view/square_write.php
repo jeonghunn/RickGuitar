061f68a237c5b4eb90c70c2e51d640ff6a2599a1
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-09 06:07:07 +0900

    test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -367,7 +367,7 @@ require_once 'pages/header.php'; ?>
     $(function () {
         $(document.body).bind('mouseup', function (e) {
             var selection;
-
+            selRange = saveSelection();
             if (window.getSelection) {
                 selection = window.getSelection();
             } else if (document.selection) {
