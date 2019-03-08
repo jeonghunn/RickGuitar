1ab5a6f85ee1df8b6a880da597270ffe2e57c0ad
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-09 02:40:19 +0900

    removed unused jquery

--- pages/footer.php
+++ pages/footer.php
@@ -14,7 +14,6 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="pages/js/bootstrap.min.js"></script>
 <script src="pages/js/bootstrap.bundle.js"></script>
 
