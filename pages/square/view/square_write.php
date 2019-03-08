ffd78537a5521b98a9d6325f38ab72eceabc41d7
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-09 04:22:27 +0900

    selectedcolor

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -197,7 +197,7 @@ require_once 'pages/header.php'; ?>
             </div>
             <div class="modal-body">
                 <div id="cp1" data-color="#6D2781">
-                    <input type="text" class="form-control" style="width:auto"/> <br>
+                    <input type="text" id="selected_color" class="form-control" style="width:auto"/> <br>
 
                 </div>
                 <script>
@@ -245,7 +245,7 @@ require_once 'pages/header.php'; ?>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php S('close') ?></button>
                 <button type="button" class="btn btn-primary"
-                        onclick=" document.execCommand('FontColor', false, document.getElementById('cp1').data-color)"><?php S('select') ?></button>
+                        onclick=" document.execCommand('FontColor', false, document.getElementById('selected_color').value)"><?php S('select') ?></button>
             </div>
         </div>
     </div>
