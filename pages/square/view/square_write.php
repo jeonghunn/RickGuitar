ff4bac37c6fdb6f072e980bc54959b0ac7c477c1
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-13 10:38:15 +0900

    picker test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -112,7 +112,7 @@ require_once 'pages/header.php'; ?>
 
             <!--     Size, Font       -->
 
-            <div id="color-picker" name="color-picker" tabindex="-1">
+            <div id="color-picker" name="color-picker" class="pickr">
 
 
             </div>
