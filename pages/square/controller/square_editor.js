059bf999432b64cad0e7f12d5d23abb9ad60442c
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-20 15:23:37 +0900

    js code has been refactored.

--- pages/square/controller/square_editor.js
+++ pages/square/controller/square_editor.js
@@ -1,4 +1,6 @@
 var editor = null;
+var picker = new Array();
+
 
 
 //Editor font control onchange
@@ -48,3 +50,213 @@ $(function () {
 
     });
 });
+
+
+function setCardBackgroundColor(cardnum, color) {
+    closeColorPickerModal();
+    document.getElementById("contents_" + cardnum).style = "background-color : " + color;
+    document.getElementById("background_" + cardnum).value = color;
+
+}
+
+function openColorPicker(index) {
+    if (picker[index].isOpen()) {
+        picker[index].hide();
+    } else {
+
+        picker[index].show();
+    }
+}
+
+function closeColorPickerModal() {
+    $('#ColorPickerModal').modal('hide');
+}
+
+
+function setCardStyle(num, style, background) {
+
+    if (background.startsWith("#")) {
+        document.getElementById("contents_" + num).style = "background-color : " + background;
+        document.getElementById("background_" + num).value = background;
+    }
+}
+
+
+function setEditorAction(action) {
+    document.execCommand(action);
+}
+
+
+function hideEditor(num) {
+    var editorid = "edit_" + num;
+    if (document.getElementById(editorid) != null && num != 0) document.getElementById(editorid).style = 'display:none;';
+}
+
+function showEditor(num) {
+    var editorid = "edit_" + num;
+    if (num != 0) document.getElementById(editorid).style = '';
+}
+
+
+function addCard() {
+
+    cardcount = cardcount + 1;
+    var cardadd = '<div class="outer" id="card_' + cardcount + '"> <div class="tablerow"><div class="squarecard  animated fadeInUp" id="contents_' + cardcount + '" onclick="activeCard(' + cardcount + ')" contentEditable="true"></div>   <input type="hidden" class="square_background" id="background_' + cardcount + '" name="background_' + cardcount + '" value=""/></div><div><br>';
+    var editor = '<div id="edit_' + cardcount + '">\n' +
+        '        <br>\n' +
+        '        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups"\n' +
+        '             style=" display: inline-block; line-height:2.8em">\n' +
+        '            <div class="btn-group btn-group" role="group" aria-label="First group">   <select name="font_name_' + cardcount + '" id="font_name_' + cardcount + '" class="custom-select custom-select-md"><?php $SQUARE_CLASS->PrintFontSelectOptions(); ?> </select> \n' +
+        '            </div>\n' +
+        '            <div class="btn-group btn-group" role="group" aria-label="First group">   <select name="font_size_' + cardcount + '" id="font_size_' + cardcount + '" class="custom-select custom-select-md"><?php $SQUARE_CLASS->PrintFontSizeOptions(); ?> </select>\n' +
+        '            </div>\n' +
+        '            <div class="btn-group btn-group" role="group" aria-label="Second group">\n' +
+        // '                <button type="button" class="btn btn btn-outline-secondary"><span class="oi oi-text"></span></button>\n' +
+        '                <button type="button" class="btn btn btn-outline-secondary" onclick="setEditorAction(\'bold\')"><span class="oi oi-bold"></span></button>\n' +
+        '                <button type="button" class="btn btn btn-outline-secondary" onclick="setEditorAction(\'italic\')"><span class="oi oi-italic"></span></button>\n' +
+        ' <button type="button" class="btn btn btn-outline-secondary"\n' +
+        '                        onclick="openColorPicker(' + cardcount + ')"><span\n' +
+        '                            class="oi oi-droplet"></span>\n' +
+        '                </button>' +
+        '            </div>\n' +
+        '            <div class="btn-group btn-group" role="group" aria-label="Third group">\n' +
+        '                <button type="button" class="btn btn btn-outline-secondary" onclick="setEditorAction(\'JustifyLeft\')"><span class="oi oi-justify-left"></span>\n' +
+        '                </button>\n' +
+        '                <button type="button" class="btn btn btn-outline-secondary" onclick="setEditorAction(\'JustifyCenter\')"><span class="oi oi-justify-center"></span>\n' +
+        '                </button>\n' +
+        '                <button type="button" class="btn btn btn-outline-secondary" onclick="setEditorAction(\'JustifyRight\')"><span class="oi oi-justify-right"></span>\n' +
+        '                </button>\n' +
+        '            </div>\n' +
+        '\n' +
+        '            <div class="btn-group btn-group" role="group" aria-label="4 group">\n' +
+        '              <button type="button" class="btn btn btn-outline-secondary" data-toggle="modal"  data-target="#ColorPickerModal"><span class="oi oi-image"></span></button>' +
+
+        '            </div>\n' +
+        '            <div class="btn-group btn-group" role="group" aria-label="4 group">\n' +
+
+
+        '            <div class="btn-group btn-group" role="group" aria-label="5 group"> <button type="button" class="btn btn btn-outline-secondary" onclick="removeCard(' + cardcount + ')"><span class="oi oi-x"></span></button>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '<div class="color-picker" id="color_picker_' + cardcount + '"></div>' +
+        '\n' +
+        '</div></div>'
+    $(cardadd + editor).insertBefore('#squarecard');
+    setEditor();
+    newPicker(cardcount);
+    hideEditor(active_card);
+    active_card = cardcount;
+
+
+    $("#font_name_" + cardcount).change(function () {
+
+        document.execCommand('FontName', false, $(this).val());
+        // alert(document.queryCommandValue("FontSize"));
+        // alert('Selected value: ' + $(this).val());
+    });
+
+
+    //Editor font control onchange
+    $("#font_size_" + cardcount).change(function () {
+
+        // var spanString = $('<span/>', {
+        //     'text': document.getSelection()
+        // }).css('font-size', $(this).val() + 'px').prop('outerHTML');
+        //
+        // document.execCommand('insertHTML', false, spanString);
+        document.execCommand('FontSize', false, $(this).val());
+        // alert(document.queryCommandValue("FontSize"));
+        // alert('Selected value: ' + $(this).val());
+    });
+
+
+    //Mouse Selection update
+    $(function () {
+        $(document.body).bind('mouseup', function (e) {
+            var selection;
+
+            if (window.getSelection) {
+                selection = window.getSelection();
+            } else if (document.selection) {
+                selection = document.selection.createRange();
+            }
+
+            //   if(selection.head)
+            //   alert(document.querySelectorAll());
+            //alert(document.queryCommandValue("FontColor"));
+            // selection.toString() !== '' && alert('"' + selection. + '" was selected at ' + e.pageX + '/' + e.pageY);
+            document.getElementById("font_size_" + cardcount).value = document.queryCommandValue("FontSize");
+            document.getElementById("font_name_" + cardcount).value = document.queryCommandValue("FontName").replace(/"([^"]+(?="))"/g, '$1');
+            //  alert(document.queryCommandValue("FontName"));
+
+        });
+    });
+
+
+}
+
+
+function newPicker(index) {
+
+    picker[index] = new Pickr({
+        el: document.getElementById("color_picker_" + index),
+        useAsButton: false,
+        default: '#42445A',
+        position: 'middle',
+
+        swatches: [
+            'rgba(244, 67, 54, 1)',
+            'rgba(233, 30, 99, 0.95)',
+            'rgba(156, 39, 176, 0.9)',
+            'rgba(103, 58, 183, 0.85)',
+            'rgba(63, 81, 181, 0.8)',
+            'rgba(33, 150, 243, 0.75)',
+            'rgba(3, 169, 244, 0.7)',
+            'rgba(0, 188, 212, 0.7)',
+            'rgba(0, 150, 136, 0.75)',
+            'rgba(76, 175, 80, 0.8)',
+            'rgba(139, 195, 74, 0.85)',
+            'rgba(205, 220, 57, 0.9)',
+            'rgba(255, 235, 59, 0.95)',
+            'rgba(255, 193, 7, 1)'
+        ],
+
+        components: {
+
+            preview: true,
+            opacity: true,
+            hue: true,
+
+            interaction: {
+                hex: true,
+                rgba: true,
+                hsva: true,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+
+    picker[index].on('change', (args) => {
+        document.execCommand('forecolor', false, args.toHEX());
+    });
+}
+
+
+function setEditor() {
+    editor = new MediumEditor('.squarecard', {
+        toolbar: {
+            buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'image', 'justifyLeft', 'justifyCenter', 'h1'],
+        },
+        buttonLabels: 'fontawesome',
+        anchor: {
+            targetCheckbox: true
+        }
+    });
+}
+
+function removeCard(num) {
+    document.getElementById("card_" + num).remove();
+}
+
--- pages/square/controller/square_write.js
+++ pages/square/controller/square_write.js
@@ -1,3 +1,10 @@
+var cardcount = 1;
+var active_card = 1;
+var status = '0';
+var isProcessing = false;
+var submitted = false;
+
+
 window.onload = function () {
 
     setEditor();
@@ -39,3 +46,168 @@ window.onbeforeunload = function () {
     }
 
 }
+
+
+function getTemp() {
+
+    if (localStorage.getItem('temp_card_count') != 0 && localStorage.getItem('temp_card_count') != null) {
+        if (confirm("<?php S('write_temp_open_ask') ?>")) {
+            //add card first
+            var CardCount = localStorage.getItem('temp_card_count');
+            for (var i = 1; i <= CardCount; i++) {
+                if (i != 1) addCard();
+                document.getElementById("contents_" + i).innerHTML = localStorage.getItem("contents_" + i);
+                setCardStyle(i, null, localStorage.getItem("background_" + i));
+            }
+
+
+        } else {
+            localStorage.clear();
+        }
+
+    }
+}
+
+
+function activeCard(cardnum) {
+
+
+    if (active_card != cardnum) {
+        //old card editor disable
+        hideEditor(active_card);
+//activeCardChange
+        active_card = cardnum;
+        //showcard
+        showEditor(active_card);
+
+    }
+
+}
+
+
+function writeAct() {
+    setProcessing(true);
+
+    var square_cards_array = [];
+
+
+    for (var i = 1; i <= cardcount; i++) {
+        if (document.getElementById("contents_" + i) == null) continue;
+        var cardvalue = ConvertForWrite(document.getElementById("contents_" + i).innerHTML);
+        //   var alignbool = document.getElementsByName("align_radio_" + i)[0].checked;
+        var align_value = "center-center";
+        var background = document.getElementById("background_" + i).value;
+
+        square_cards_array.push({align: align_value, content: cardvalue, background: background});
+
+    }
+
+
+    if (!checkWrote()) {
+        alert('내용을 입력해주세요.');
+
+
+        setProcessing(false);
+        return false;
+    }
+
+    var square_cards = JSON.stringify(square_cards_array);
+
+
+    $.ajax({
+        type: "POST",
+        url: "<?php echo getAPIUrlS() ?>",
+        data: {
+            "a": "square_write",
+            "apiv": "<?php echo getAPIVersion()?>",
+            "api_key": "<?php echo getAPIKey()?>",
+            "auth": "<?php echo getUserAuth()?>",
+            "status": status,
+            "keep_square": document.getElementById("keep_square").checked ? 1 : 0,
+            "type": "square",
+            "square_cards": square_cards
+        },
+        success: function (data) {
+            setProcessing(false);
+            if (data.indexOf('success') >= 0) {
+
+                localStorage.clear();//delete temp
+                submitted = true; //success submit
+                var Result = JSON.parse(data);
+
+                location.href = Result['square_key'];
+
+
+            } else {
+                alert('<?php S('
+                error_unknown_error
+                ') ?>'
+            )
+                ;
+
+
+            }
+        },
+
+        error: function (jqXHR) {
+            setProcessing(false);
+            alert('<?php S('
+            error_unknown_error
+            ') ?>'
+        )
+            ;
+        }
+
+
+    });
+
+
+}
+
+
+function checkPreventClose() {
+    console.log(((checkWrote()) && (!submitted)));
+    return ((checkWrote()) && (!submitted));
+}
+
+
+function checkWrote() {
+    return document.getElementById("contents_1").innerHTML != '';
+}
+
+
+function setStatus(s) {
+    status = s;
+    if (status == '0') document.getElementById("dropdownMenuButton").innerHTML = "<?php S('write_public') ?>";
+    if (status == 'unregistered') document.getElementById("dropdownMenuButton").innerHTML = "<?php S('write_private') ?>";
+}
+
+
+function ConvertForWrite(content) {
+    content = replaceAll(content, "<", "{[");
+    content = replaceAll(content, ">", "]}");
+    return content;
+}
+
+
+function replaceAll(str, searchStr, replaceStr) {
+    return str.split(searchStr).join(replaceStr);
+}
+
+function setProcessing(status) {
+    isProcessing = status;
+    setbuttonstatus(!status);
+}
+
+
+function setbuttonstatus(status) {
+
+    if (status) {
+        $('button').prop('disabled', false);
+    } else {
+        $('button').prop('disabled', true);
+    }
+
+}
+
+
