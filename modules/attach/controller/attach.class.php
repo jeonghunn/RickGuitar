667eb0adcdff4e021460b2d6bcb3b85ab3aa412e
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-21 22:44:42 +0900

    array test

--- modules/attach/controller/attach.class.php
+++ modules/attach/controller/attach.class.php
@@ -40,7 +40,7 @@ $all_result = true;
 
 
             $upload_result = move_uploaded_file($_FILES['uploadedfile']['tmp_name'][$i], $target_path);
-            echo $upload_result . "dfs";
+            if ($upload_result) echo $upload_result . "dfs";
             if ($upload_result) $result = Model_Attach_addAttch($page_srl, $category, $doc_srl, $user_srl, $kind, $filename, $extension, $filevalue, $size, $status);
         }
 
