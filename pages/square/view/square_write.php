30361b8d22978c04f7e4274f76bec727b6ba8352
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-21 20:20:23 +0900

    gallery test

--- pages/square/view/square_write.php
+++ pages/square/view/square_write.php
@@ -164,6 +164,19 @@ require_once 'pages/header.php'; ?>
                     <tbody class="files"></tbody>
                 </table>
             </form>
+
+            <!-- The blueimp Gallery widget -->
+            <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" data-filter=":even">
+                <div class="slides"></div>
+                <h3 class="title"></h3>
+                <a class="prev">‹</a>
+                <a class="next">›</a>
+                <a class="close">×</a>
+                <a class="play-pause"></a>
+                <ol class="indicator"></ol>
+            </div>
+
+
         </div>
 
 
