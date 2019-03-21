c2ee1e15249851ae8f5cf182e14bc9f278e27bc9
JHRunning <jeonghunn1@gmail.com>
2019-03-22 01:09:56 +0900

    url call bug has been fixed

--- modules/attach/controller/attach.class.php
+++ modules/attach/controller/attach.class.php
@@ -47,7 +47,7 @@ $all_result = true;
 
             if ($upload_result == true) {
                 $result = Model_Attach_addAttch($page_srl, $category, $doc_srl, $user_srl, $kind, $filename, $extension, $filevalue, $size, $status);
-                array_push($result_array['files'], array("name" => $filename, "size" => $size, "url" => getCoreUrl(true) . "$target_path", "deleteUrl" => getCoreUrl(true) . "$target_path", "deleteType" => "DELETE"));
+                array_push($result_array['files'], array("name" => $filename, "size" => $size, "url" => getSiteAddress() . "$target_path", "deleteUrl" => getSiteAddress() . "$target_path", "deleteType" => "DELETE"));
             } else {
                 array_push($result_array['files'], array("name" => $filename, "size" => $size, "error" => $error));
             }
