0f76cec7fcf96ceee81ef5be026ce979c210221a
JHRunning <jeonghunn1@gmail.com>
2019-03-24 00:55:28 +0900

    New API : Delete Attach

--- core/base.php
+++ core/base.php
@@ -44,6 +44,13 @@ function getAPIUrlS()
     return getCoreUrl(isSecure()) . "api.php";
 }
 
+function isSecure()
+{
+    return
+        (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+        || $_SERVER['SERVER_PORT'] == 443;
+}
+
 function getAPISUrl()
 {
     return getCoreUrl(true) . "api.php";
