0f953daf74877c711ddd8a4a1156bed04897ca4b
Junghoon Lee <jeonghunn1@gmail.com>
2019-03-24 10:55:48 +0900

    square_collection api has been removed temporally

--- modules/square/square.api.php
+++ modules/square/square.api.php
@@ -8,7 +8,7 @@
 
 
 if ($ACTION == "square_main") $SquareAPI->API_getMain($user_srl);
-if ($ACTION == "square_collection") $SquareAPI->API_getCollection($user_srl);
+//if ($ACTION == "square_collection") $SquareAPI->API_getCollection($user_srl);
 if($ACTION == "square_write") $SquareAPI -> API_Write($user_srl);
 if($ACTION == "square_read") $SquareAPI -> API_Read($user_srl);
 if($ACTION == "square_status_update") $SquareAPI -> API_DocStatusUpdate($user_srl);
