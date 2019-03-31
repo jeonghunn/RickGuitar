90dd47ea64ac3cf50ebfe173baaf8b47f05dccb3
JHRunning <jeonghunn1@gmail.com>
2019-04-01 01:53:50 +0900

    update uploader

--- pages/square/controller/square_uploader.js.php
+++ pages/square/controller/square_uploader.js.php
@@ -41,7 +41,7 @@ $(function () {
         disableImageResize: /Android(?!.*Chrome)|Opera/
             .test(window.navigator.userAgent),
         maxFileSize: 31457280,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|pdf|hwp|zip|rar|mp3|flac|mp4|avi|docx|doc|ppt|pptx|xls|xlsx)$/i
     });
     // Upload server status check for browsers with CORS support:
     if ($.support.cors) {
