3df26e01e7685626fc11348b739132d7df8a966b
Junghoon Lee <jeonghunn1@gmail.com>
2019-04-03 15:57:42 +0900

    New Page : add_creator_1

--- pages/square/view/square_add_creator_1.php
+++ pages/square/view/square_add_creator_1.php
@@ -54,7 +54,7 @@
         <p class="lead">어떤 형태의 카드를 만들 지 템플릿을 만들어주세요.</p>
         <hr class="my-4">
         <p>사용자마다 다른 값이 나올수 있는 부분은 변수 처리해야합니다. [[gender]] 와 같이 입력하면 추후에 [[gender]] 부분은 다른 값으로 대체하게 됩니다.</p>
-        <a class="btn btn-dark btn-lg" href="#" role="button">카드 템플릿 만들기</a>
+        <a class="btn btn-dark btn-lg" href="write?mode=template" role="button">카드 템플릿 만들기</a>
     </div>
 
 </div>
