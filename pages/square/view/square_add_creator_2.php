becbc46c4b618c6fc461209c0d50885b33d1b232
Junghoon Lee <jeonghunn1@gmail.com>
2019-04-04 12:55:17 +0900

    square_Add_creator_2 works

--- pages/square/view/square_add_creator_2.php
+++ pages/square/view/square_add_creator_2.php
@@ -53,7 +53,7 @@
         <h1 class="display-4">Step 2 : 기본정보 입력</h1>
         <p class="lead">생성할 생성기의 기본 정보를 입력해 주세요.</p>
     </div>
-
+    <form action="add_creator_3" method="post" id="creatorform">
 
     <div class="row">
         <div class="col-sm-6">
@@ -62,29 +62,29 @@
                 <div class="card-body">
                     <div class="input-group input-group-lg">
                         <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-lg">생성기 이름</span>
+                            <span class="input-group-text" id="creator_name">생성기 이름</span>
                         </div>
                         <input type="text" class="form-control" aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-lg">
+                               aria-describedby="creator_name" id="creator_name_input">
                     </div>
                     <br>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-default">템블릿 KEY</span>
+                            <span class="input-group-text" id="inputGroup-sizing-default">템플릿 KEY</span>
                         </div>
                         <input type="text" class="form-control" aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-default">
+                               aria-describedby="inputGroup-sizing-default" id="template_square_key_input">
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="inputGroup-sizing-default">색상 HEX 코드</span>
                         </div>
                         <input type="text" class="form-control" aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-default">
+                               aria-describedby="inputGroup-sizing-default" id="color_input">
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">생성기에 대한 설명</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <label for="creator_des_textarea">생성기에 대한 설명</label>
+                        <textarea class="form-control" id="creator_des_textarea" rows="3"></textarea>
                     </div>
 
                 </div>
@@ -95,11 +95,36 @@
 
             <p>- 템플릿 KEY는 템플릿으로 사용할 페이지의 KEY 값을 의미합니다. 입력되어 있으면 변경할 필요 없습니다.</p>
             <p>- 색상 HEX 코드는 생성기의 주요한 색으로 사용할 컬러 코드(예 : #000000)를 이용할 수 있습니다. 필수 입력 사항은 아닙니다.</p>
-            <button type="button" class="btn btn-dark btn-lg">다음</button>
+            <button type="button" class="btn btn-dark btn-lg" onclick="postAct()">다음</button>
         </div>
     </div>
 
 
+    </form>
+
 
 
 </div>
+
+
+<script>
+
+    function postAct() {
+
+        var creatorform = document.getElementById("creatorform");
+        var name = document.getElementById("creator_name_input").value;
+        var template_square_key = document.getElementById("template_square_key_input").value;
+        var color = document.getElementById("color_input").value;
+        var description = document.getElementById("creator_des_textarea").value;
+
+        if (name == null || template_square_key == null || description == null) {
+            alert("이름, 템플릿 키, 설명 항목을 입력해야 합니다.");
+            return false;
+        } else {
+            creatorform.submit();
+        }
+
+
+    }
+
+</script>
