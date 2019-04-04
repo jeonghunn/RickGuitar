620c8ae97528e9027599583ba90c8a5f5f1d3453
Junghoon Lee <jeonghunn1@gmail.com>
2019-04-04 12:37:04 +0900

    square_Add_creator_2 description

--- pages/square/view/square_add_creator_2.php
+++ pages/square/view/square_add_creator_2.php
@@ -54,46 +54,52 @@
         <p class="lead">생성할 생성기의 기본 정보를 입력해 주세요.</p>
     </div>
 
-    <div class="card text-center align-self-center">
 
-        <div class="card-body">
-            <div class="input-group input-group-lg">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-lg">생성기 이름</span>
-                </div>
-                <input type="text" class="form-control" aria-label="Sizing example input"
-                       aria-describedby="inputGroup-sizing-lg">
-            </div>
-            <br>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">템블릿 KEY</span>
-                </div>
-                <input type="text" class="form-control" aria-label="Sizing example input"
-                       aria-describedby="inputGroup-sizing-default">
-            </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">색상 HEX 코드</span>
-                </div>
-                <input type="text" class="form-control" aria-label="Sizing example input"
-                       aria-describedby="inputGroup-sizing-default">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">생성기에 대한 설명</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card align-self-center">
 
+                <div class="card-body">
+                    <div class="input-group input-group-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-lg">생성기 이름</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input"
+                               aria-describedby="inputGroup-sizing-lg">
+                    </div>
+                    <br>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">템블릿 KEY</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input"
+                               aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">색상 HEX 코드</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input"
+                               aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">생성기에 대한 설명</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+
+                </div>
+            </div>
         </div>
-    </div>
+        <div class="col-sm-6">
 
 
-    <div class="card text-center align-items-center">
-
-        <div class="card-body">
             <p>- 템플릿 KEY는 템플릿으로 사용할 페이지의 KEY 값을 의미합니다. 입력되어 있으면 변경할 필요 없습니다.</p>
             <p>- 색상 HEX 코드는 생성기의 주요한 색으로 사용할 컬러 코드(예 : #000000)를 이용할 수 있습니다. 필수 입력 사항은 아닙니다.</p>
+
         </div>
     </div>
+
+
+
 
 </div>
