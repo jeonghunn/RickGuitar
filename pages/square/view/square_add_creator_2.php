614359ee24943e372dd17456d4b4412ef2cfc4a4
Junghoon Lee <jeonghunn1@gmail.com>
2019-04-04 10:32:24 +0900

    square_Add_creator_2

--- pages/square/view/square_add_creator_2.php
+++ pages/square/view/square_add_creator_2.php
@@ -69,7 +69,7 @@
             <input type="text" class="form-control" aria-label="Sizing example input"
                    aria-describedby="inputGroup-sizing-default">
         </div>
-
+        <br>
         <div class="form-group">
             <label for="exampleFormControlTextarea1">생성기에 대한 설명</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
