1553eaf4cbe4466dd64e53415f9ecde9731a88b5
Junghoon Lee <jeonghunn1@gmail.com>
2019-06-01 03:34:25 +0900

    config update

--- config.php
+++ config.php
@@ -4,18 +4,14 @@ ini_set('memory_limit', '1024M');
 
 $HELLO_CORE_VERSION = "1.0";
 $CORE_VERSION = "3.18.514";
-$DEVELOPMENT_MODE = true;
 $DEVELOPMENT_SERVER_URL = "unopenedbox.com/develop/square/";
 $SERVER_URL = "s9uare.com/";
 $MAIN_URL = "http://s9uare.com/";
 $MAIN_API_URL = "http://s9uare.com/api.php";
 $CLIENT_SERVER_IP_ADDRESS = "52.78.110.116";
 
-//DATABASE
-$DB_NAME = "square";
-$DB_SERVER_PATH = "localhost";
-$DB_USERNAME = "root";
-$DB_PASSWORD = "root";
+//Database
+require_once 'db_config.php';
 
 //Import Modules
 $IMPORT_MODULE = array('settings', 'main', 'account', 'board', 'page', 'notification', 'attach', 'square');
