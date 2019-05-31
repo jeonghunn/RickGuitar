3c0ccae1075342442a672d4d0678fdd1d484c4c4
Junghoon Lee <jeonghunn1@gmail.com>
2019-05-31 20:11:59 +0900

    restriction test

--- pages/square/controller/square_editor.js.php
+++ pages/square/controller/square_editor.js.php
@@ -34,7 +34,9 @@ interact('.resize-drag')
         restrict: {
             restriction: "parent",
             endOnly: true,
-            elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+            elementRect: {top: 0, left: 0, bottom: 1, right: 1},
+            width: 440,
+            height: 440
         },
 
     })
