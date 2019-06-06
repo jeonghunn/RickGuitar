832a001a6f35693368dcff6695aa85993fdd0631
Junghoon Lee <jeonghunn1@gmail.com>
2019-06-06 22:36:58 +0900

    sign in feature

--- pages/login.php
+++ pages/login.php
@@ -46,48 +46,22 @@ importHeader(null);
     }
 </style>
 
-<form class="form-signin">
-    <img class="mb-4" src="{{ site.baseurl }}/docs/{{ site.docs_version }}/assets/brand/bootstrap-solid.svg" alt=""
-         width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <label for="inputEmail" class="sr-only">Email address</label>
+<form class="form-signin" id="signinform" name="signinform"
+      action='<?php echo getClientUrl(true) . "index.php?a=loginact'"; ?>'>
+    <h1 class="h3 mb-3 font-weight-normal"><?php S('sign_in'); ?></h1>
+    <label for="inputEmail" class="sr-only"><?php S('email'); ?></label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
+    <label for="inputPassword" class="sr-only"><?php S('password'); ?></label>
     <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
     <div class="checkbox mb-3">
         <label>
-            <input type="checkbox" value="remember-me"> Remember me
+            <input type="checkbox" value="remember-me"> <?php S('keep_signed_in'); ?>
         </label>
     </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017-{{ site.time | date: "%Y" }}</p>
+    <button class="btn btn-lg btn-primary btn-block" type="submit"
+            onclick="validcheckandsubmit();"><?php S('sign_in'); ?></button>
 </form>
 
-
-
-
-    <paper-material elevation="1" id="cardpaper" style="margin-top: 3%;  height: auto;">
-
-      <form id="signinform" name="signinform" method='post'  action='<?php echo getClientUrl(true)."index.php?a=loginact'"; ?>' >
-        <h2 class="form-signin-heading"><?php S('sign_in'); ?></h2>
-
-          <paper-input type="email" name='email_form' id="email_form" placeholder="<?php S('email');?>" required autofocus></paper-input>
-<paper-input type="password" name='password_form' id="password_form"  placeholder="<?php S('password');?>" required></paper-input>
-          <input id="email" name="email" style=" display: none;"/>
-          <input id="password" name="password" style=" display: none;"/>
-
-          <div><paper-checkbox id="keep_signed_in_form""><?php S('keep_signed_in');?></paper-checkbox><input type="hidden" id="keep_signed_in" name="keep_signed_in"/><a href="signup" style="float:right;"><?php S('sign_up');?></a></div>
-          <br><br>
-        <paper-button raised onclick="validcheckandsubmit();"><?php S('sign_in');?></paper-button>
-
-
-      </form>
-
-    </paper-material>
-
-<paper-toast text="<?php S('sign_in_blank_des');?>" id="sign_in_blank_error_toast"></paper-toast>
-
-<script>
 
 
     $("#signinform").keypress(function(event) {
